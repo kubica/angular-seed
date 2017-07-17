5c5c8d33d63cca1f3ea048f0e14b9989192f0f91
Tommy Kubica <tommy.kubica@tu-dresden.de>
2017-07-18 00:09:09 +0200

    Minimal reproduction of the problem

--- src/client/app/app.component.ts
+++ src/client/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Config } from './shared/config/env.config';
 import './operators';
+import { TranslateService } from '@ngx-translate/core';
 
 /**
  * This class represents the main application component.
@@ -12,7 +13,10 @@ import './operators';
   styleUrls: ['app.component.css'],
 })
 export class AppComponent {
-  constructor() {
-    console.log('Environment config', Config);
-  }
+	constructor(translate: TranslateService) {
+		console.log('Environment config', Config);
+
+		translate.setDefaultLang('en');
+		translate.use('en');
+	}
 }
--- src/client/app/app.module.ts
+++ src/client/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { APP_BASE_HREF } from '@angular/common';
-import { HttpModule } from '@angular/http';
+import { HttpModule, Http } from '@angular/http';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
@@ -9,9 +9,21 @@ import { AboutModule } from './about/about.module';
 import { HomeModule } from './home/home.module';
 import { SharedModule } from './shared/shared.module';
 
+import { TranslateModule, TranslateLoader, TranslatePipe } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+export function createTranslateLoader(http: Http) {
+	return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 @NgModule({
-  imports: [BrowserModule, HttpModule, AppRoutingModule, AboutModule, HomeModule, SharedModule.forRoot()],
+  imports: [BrowserModule, TranslateModule.forRoot({
+			loader: {
+				provide: TranslateLoader,
+				useFactory: (createTranslateLoader),
+				deps: [Http]
+			}
+		}), HttpModule, AppRoutingModule, AboutModule, HomeModule, SharedModule.forRoot()],
   declarations: [AppComponent],
   providers: [{
     provide: APP_BASE_HREF,
